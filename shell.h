38ef1885178215160602c5a5f12d426d47426baf
tajirri <clarencewagah@gmail.com>
2023-12-15 00:22:22 -0800

    task 1

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#endif
